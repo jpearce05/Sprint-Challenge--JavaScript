8008990d42f13b10d5094689c85c9dbb3e5236bb
jpearce05 <joseph@zotta.com>
2020-05-22 10:31:06 -0700

    add Closure

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -63,9 +63,6 @@ console.log(displayNames);
 
 
 
-
-
-
 /* Request 2: .map()
 
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
@@ -132,9 +129,11 @@ The zoos need to know their total animal population across the United States. Fi
 
 
 // Version 1
-const populationTotal = 0;
-console.log(populationTotal);
+const populationTotal = zooAnimals.reduce(function(accumulator, item) {
+  return accumulator + item.population;
+},0);
 
+console.log(populationTotal);
 
 
 
@@ -147,6 +146,10 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a,b,cb) {
+  return cb(a,b);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -154,13 +157,23 @@ console.log(populationTotal);
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
-
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 /*
--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,19 @@ myFunction();
 
 // Explanation: 
 
+// the nested function can access the variable internal because it resides inside the function
+
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function sumation(param) {
+ let sum = 0;
+ for (let i = 1; i<=param; i++) {
+  //  sum = sum + i;
+   sum +=i;
+ }
+ return sum;
+}
+console.log(sumation(4));
